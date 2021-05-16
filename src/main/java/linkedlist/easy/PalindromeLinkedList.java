e0295addef8f67b3e45b9335a855d7b6f78aa720
Vladimir <sirbuvova4@gmail.com>
2021-05-16 12:30:29 +0000

    added second aproach to problem -> Palindrome Linked List

--- src/main/java/linkedlist/easy/PalindromeLinkedList.java
+++ src/main/java/linkedlist/easy/PalindromeLinkedList.java
@@ -1,5 +1,8 @@
 package linkedlist.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PalindromeLinkedList {
 
     // Aproach 1 -> every element of node append to string builder and then check if is equal to its reverse
@@ -10,5 +13,26 @@ public class PalindromeLinkedList {
             head = head.getNext();
         }
         return sb.toString().equals(sb.reverse().toString());
+    }
+
+    // Aproach 2
+    public boolean isPalindrome1(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(current.getVal());
+            current = current.getNext();
+        }
+
+        current = head;
+        int index = list.size() - 1;
+
+        while (current != null & index >= 0) {
+            if (current.getVal() != list.get(index))
+                return false;
+            current = current.getNext();
+            index--;
+        }
+        return true;
     }
 }
