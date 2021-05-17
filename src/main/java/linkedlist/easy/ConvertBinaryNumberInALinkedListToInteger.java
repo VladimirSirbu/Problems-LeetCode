e533ed2c71fe4eb218228bcd8903ea571f14a815
Vladimir <sirbuvova4@gmail.com>
2021-05-17 15:08:55 +0000

    added new approaches to previous problem

--- src/main/java/linkedlist/easy/ConvertBinaryNumberInALinkedListToInteger.java
+++ src/main/java/linkedlist/easy/ConvertBinaryNumberInALinkedListToInteger.java
@@ -1,7 +1,9 @@
 package linkedlist.easy;
 
 public class ConvertBinaryNumberInALinkedListToInteger {
-    public int getDecimalValue(ListNode head) {
+
+    // Aproach 1 (own)
+    public int getDecimalValue1(ListNode head) {
         StringBuilder sb = new StringBuilder();
         while(head != null) {
             sb.append(head.val);
@@ -15,5 +17,25 @@ public class ConvertBinaryNumberInALinkedListToInteger {
             decimalNumber += Integer.parseInt(String.valueOf(binaryStr.charAt(i))) * Math.pow(2, pow--);
 
         return decimalNumber;
+    }
+
+    // Aproach 2
+    public int getDecimalValue2(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = num * 2 + head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+
+    // Aproach 3
+    public int getDecimalValue3(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = (num << 1) | head.next.val;
+            head = head.next;
+        }
+        return num;
     }
 }
