beab3ae20128bbad7e8ecfb204382a5965b900c4
Vladimir <sirbuvova4@gmail.com>
2021-05-17 23:22:54 +0000

    added new approaches to problem - Middle of the Linked List

--- src/main/java/linkedlist/easy/MiddleOfTheLinkedList.java
+++ src/main/java/linkedlist/easy/MiddleOfTheLinkedList.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MiddleOfTheLinkedList {
 
     // Aproach 1 (own)
-    public ListNode middleNode(ListNode head) {
+    public ListNode middleNode1(ListNode head) {
         List<ListNode> list = new ArrayList<>();
         ListNode current = head;
         while (current != null) {
@@ -14,5 +14,27 @@ public class MiddleOfTheLinkedList {
             current = current.next;
         }
         return list.get(list.size() / 2);
+    }
+
+    // Aproach 2 (LeetCode)
+    public ListNode middleNode2(ListNode head) {
+        ListNode[] arr = new ListNode[100];
+        int count = 0;
+        while (head != null) {
+            arr[count++] = head;
+            head = head.next;
+        }
+
+        return arr[count / 2];
+    }
+
+    // Aproach 3 (LeetCode)
+    public ListNode middleNode3(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
